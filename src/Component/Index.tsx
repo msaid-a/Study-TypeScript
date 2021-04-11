db71b068ccddd9060e686345afa39b8842e4abc5
msaid-a <muhammad.arrafi@fast-8.com>
2021-04-11 17:34:29 +0700

    change icon + change background

--- src/Component/Index.tsx
+++ src/Component/Index.tsx
@@ -1,11 +1,6 @@
 import React, {useState, useEffect} from 'react'
-// import axios from 'axios'
-import {useSelector, useDispatch} from 'react-redux'
-import {StateDefault} from '../redux/covidReducer'
-import {getData, getDataGlobal, getDetailCovid} from '../action/covidAction'
-import {Button, Form, Input, Icon, Checkbox} from 'antd'
+
 import { FormComponentProps } from "antd/lib/form";
-import Headers from './Headers'
 import Data from './Data'
 import ProvinceData from './ProvinceData'
 
@@ -18,10 +13,6 @@ type props = {
 
 
 const Training: React.FC<props & FormComponentProps> = ({form, tittle}) => {
-    const covidData = useSelector<StateDefault, StateDefault["covid1"]>((state) => state.covid1)
-    const covidDataGlobal = useSelector<StateDefault, StateDefault["covidWorld"]>((state) => state.covidWorld)
-    const covidDataDetail = useSelector<StateDefault>((state) => state)
-
 
     
 
